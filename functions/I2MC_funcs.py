cc4d9fb6959cd7982b98f4f30ce4b4ddfdb1d3fc
Diederick Niehorster <dcnieho@gmail.com>
2020-03-02 14:56:22 +0100

    now with Steffen interp!

--- functions/I2MC_funcs.py
+++ functions/I2MC_funcs.py
@@ -496,9 +496,9 @@ def findInterpWins(xpos, ypos, missing, windowtime, edgesamples, freq, maxdisp):
     for s, e in zip(missStart, missEnd):
         notAllowed[range(s,e+1)] = False
     
-    return notAllowed
+    return missStart,missEnd
 
-def windowedInterpolate(xpos, ypos, missing, notAllowed, dev=False):
+def windowedInterpolate(xpos, ypos, missing, missStart, missEnd, edgesamples, dev=False):
     """
     Interpolates the missing data, and removes areas which are not allowed 
     to be interpolated
@@ -526,18 +526,25 @@ def windowedInterpolate(xpos, ypos, missing, notAllowed, dev=False):
     >>>
     """
     # Do the interpolating
-    notMissing = np.invert(missing)
-    newX = np.arange(len(xpos))
+    for p in range(len(missStart)):
+        # make vector of all samples in this window
+        outWin = np.arange(missStart[p],missEnd[p]+1)
     
-    # Interpolate x values
-    interpolater = interp.PchipInterpolator(newX[notMissing],xpos[notMissing])
-    xi = interpolater(newX)
-    xi[notAllowed] = np.nan
-    
-    # Interpolate y values
-    interpolater = interp.PchipInterpolator(newX[notMissing],ypos[notMissing])
-    yi = interpolater(newX)
-    yi[notAllowed] = np.nan
+        # get edge samples: where no missing data was observed
+        # also get samples in window where data was observed
+        outWinNotMissing = np.invert(missing[outWin])
+        validsamps  = np.concatenate((outWin[0]+np.arange(-edgesamples,0), outWin[outWinNotMissing], outWin[-1]+np.arange(1,edgesamples+1)))
+        
+        # get valid values: where no missing data was observed
+        validx      = xpos[validsamps];
+        validy      = ypos[validsamps];
+        
+        # do Steffen interpolation, update xpos, ypos
+        xpos[outWin]= steffenInterp(validsamps,validx,outWin)
+        ypos[outWin]= steffenInterp(validsamps,validy,outWin)
+        
+        # update missing: hole is now plugged
+        missing[outWin] = True
     
     # plot interpolated data before 
     if dev:
@@ -549,7 +556,120 @@ def windowedInterpolate(xpos, ypos, missing, notAllowed, dev=False):
         ax2.scatter(newX[notMissing], ypos[notMissing], s = 2, color = 'r')
         ax2.scatter(newX[missing], yi[missing], s = 25, color = 'b')
 
-    return xi, yi
+    return xpos, ypos, missing
+
+# =============================================================================
+# interpolator
+# =============================================================================
+def steffenInterp(x, y, xi):
+    # STEFFEN 1-D Steffen interpolation
+    #    steffenInterp[X,Y,XI] interpolates to find YI, the values of the
+    #    underlying function Y at the points in the array XI, using
+    #    the method of Steffen.  X and Y must be vectors of length N.
+    #
+    #    Steffen's method is based on a third-order polynomial.  The
+    #    slope at each grid point is calculated in a way to guarantee
+    #    a monotonic behavior of the interpolating function.  The 
+    #    curve is smooth up to the first derivative.
+
+    # Joe Henning - Summer 2014
+    # edited DC Niehorster - Summer 2015
+
+    # M. Steffen
+    # A Simple Method for Monotonic Interpolation in One Dimension
+    # Astron. Astrophys. 239, 443-450 [1990]
+    n = len(x)
+
+    # calculate slopes
+    yp = np.zeros(n)
+
+    # first point
+    h1 = x[1] - x[0]
+    h2 = x[2] - x[1]
+    s1 = (y[1] - y[0])/h1
+    s2 = (y[2] - y[1])/h2
+    p1 = s1*(1 + h1/(h1 + h2)) - s2*h1/(h1 + h2)
+    if p1*s1 <= 0:
+        yp[0] = 0
+    elif np.abs(p1) > 2*np.abs(s1):
+        yp[0] = 2*s1
+    else:
+        yp[0] = p1
+
+
+    # inner points
+    for i in range(1,n-1):
+        hi = x[i+1] - x[i]
+        him1 = x[i] - x[i-1]
+        si = (y[i+1] - y[i])/hi
+        sim1 = (y[i] - y[i-1])/him1
+        pi = (sim1*hi + si*him1)/(him1 + hi)
+       
+        if sim1*si <= 0:
+            yp[i] = 0
+        elif (np.abs(pi) > 2*np.abs(sim1)) or (np.abs(pi) > 2*np.abs(si)):
+            a = np.sign(sim1)
+            yp[i] = 2*a*np.min([np.abs(sim1),np.abs(si)])
+        else:
+            yp[i] = pi
+        
+
+
+    # last point
+    hnm1 = x[n-1] - x[n-2]
+    hnm2 = x[n-2] - x[n-3]
+    snm1 = (y[n-1] - y[n-2])/hnm1
+    snm2 = (y[n-2] - y[n-3])/hnm2
+    pn = snm1*(1 + hnm1/(hnm1 + hnm2)) - snm2*hnm1/(hnm1 + hnm2)
+    if pn*snm1 <= 0:
+         yp[n-1] = 0
+    elif np.abs(pn) > 2*np.abs(snm1):
+         yp[n-1] = 2*snm1
+    else:
+         yp[n-1] = pn
+
+
+    yi = np.zeros(xi.size)
+    for i in range(len(xi)):
+        # Find the right place in the table by means of a bisection.
+        # do this instead of search with find as the below now somehow gets
+        # better optimized by matlab's JIT [runs twice as fast].
+        klo = 1
+        khi = n
+        while khi-klo > 1:
+            k = int(np.fix((khi+klo)/2.0))
+            if x[k] > xi[i]:
+                khi = k
+            else:
+                klo = k
+            
+        
+        
+        # check if requested output is in input, so we can just copy
+        if xi[i]==x[klo]:
+             yi[i] = y[klo]
+             continue
+        elif xi[i]==x[khi]:
+             yi[i] = y[khi]
+             continue
+        
+        
+        h = x[khi] - x[klo]
+        s = (y[khi] - y[klo])/h
+
+        a = (yp[klo] + yp[khi] - 2*s)/h/h
+        b = (3*s - 2*yp[klo] - yp[khi])/h
+        c = yp[klo]
+        d = y[klo]
+
+        t = xi[i] - x[klo]
+        # Use Horner's scheme for efficient evaluation of polynomials
+        # y = a*t*t*t + b*t*t + c*t + d
+        yi[i] = d + t*(c + t*(b + t*a))
+
+    return yi
+
+
 
 # =============================================================================
 # Clustering functions
@@ -1282,17 +1402,17 @@ def I2MC(gazeData, options = {}):
     # =============================================================================
     # get interpolation windows for average and individual eye signals
     print('\tSearching for valid interpolation windows')
-    missingn = findInterpWins(xpos, ypos, missing, par['windowtimeInterp'], par['edgeSampInterp'], par['freq'], par['maxdisp'])
+    missStart,missEnd = findInterpWins(xpos, ypos, missing, par['windowtimeInterp'], par['edgeSampInterp'], par['freq'], par['maxdisp'])
     if q2Eyes:
-        llmissn = findInterpWins(data['L_X'], data['L_Y'], llmiss, par['windowtimeInterp'], par['edgeSampInterp'], par['freq'], par['maxdisp']);
-        rrmissn = findInterpWins(data['R_X'], data['R_Y'], rrmiss, par['windowtimeInterp'], par['edgeSampInterp'], par['freq'], par['maxdisp']);
+        llmissStart,llmissEnd = findInterpWins(data['L_X'], data['L_Y'], llmiss, par['windowtimeInterp'], par['edgeSampInterp'], par['freq'], par['maxdisp']);
+        rrmissStart,rrmissEnd = findInterpWins(data['R_X'], data['R_Y'], rrmiss, par['windowtimeInterp'], par['edgeSampInterp'], par['freq'], par['maxdisp']);
     
     # Use Steffen interpolation and replace values
     print('\tReplace interpolation windows with Steffen interpolation')
-    xpos, ypos= windowedInterpolate(xpos, ypos, missing, missingn, par['dev_interpolation'])
+    xpos, ypos, missingn = windowedInterpolate(xpos, ypos, missing, missStart, missEnd, par['edgeSampInterp'], par['dev_interpolation'])
     if q2Eyes:
-        llx, lly = windowedInterpolate(data['L_X'], data['L_Y'], data['left_missing'], llmissn, par['dev_interpolation'])
-        rrx, rry = windowedInterpolate(data['R_X'], data['R_Y'], data['right_missing'], rrmissn, par['dev_interpolation'])       
+        llx, lly,llmissn = windowedInterpolate(data['L_X'], data['L_Y'], data['left_missing'], llmissStart, llmissEnd, par['edgeSampInterp'], par['dev_interpolation'])
+        rrx, rry,rrmissn = windowedInterpolate(data['R_X'], data['R_Y'], data['right_missing'], rrmissStart, rrmissEnd, par['edgeSampInterp'], par['dev_interpolation'])       
         
     # =============================================================================
     # 2-MEANS CLUSTERING
