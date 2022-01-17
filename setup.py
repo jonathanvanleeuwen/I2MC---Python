f4ee1d35845347e60b12b8182221ac109fd0c575
Diederick Niehorster <dcnieho@gmail.com>
2022-01-17 12:52:41 +0100

    list all imported packages as install_requirements

--- setup.py
+++ setup.py
@@ -33,4 +33,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=['os','numpy','matplotlib','time','math','scipy','copy','warnings']
 )
